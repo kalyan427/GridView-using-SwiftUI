932d3e85529073740a16c73682660f0f9c66b0c6
Kalyan <pvkalyan427@gmail.com>
2022-12-18 12:03:10 -0500

    Initial Commit

---
+++ Apple Frameworks/Apple Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple Frameworks
+//
+//  Created by kalyan . on 12/18/22.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
