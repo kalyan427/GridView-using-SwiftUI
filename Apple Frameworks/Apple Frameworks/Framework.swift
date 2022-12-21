14fb15322504f670115350f2f045f1966f39ebd9
Kalyan <pvkalyan427@gmail.com>
2022-12-20 19:56:28 -0500

    Completed grid view

--- Apple Frameworks/Apple Frameworks/Framework.swift
+++ Apple Frameworks/Apple Frameworks/Framework.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Hashable, Identifiable {
+    let id = UUID()
     var name: String
     var imageName: String
     var urlString: String
