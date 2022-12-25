dd9205305be6bbf326c005dec02927a70fd5f1fc
Kalyan <pvkalyan427@gmail.com>
2022-12-24 19:07:08 -0500

    Detail view

---
+++ Apple Frameworks/Apple Frameworks/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  Apple Frameworks
+//
+//  Created by kalyan . on 12/24/22.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Learn More")
+    }
+}
---
+++ Apple Frameworks/Apple Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkDetailView.swift
+//  Apple Frameworks
+//
+//  Created by kalyan . on 12/24/22.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
